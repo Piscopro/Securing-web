97dbf84db7740cfd5ff52ba5d43c11643578c706
Piscopro <80594224+Piscopro@users.noreply.github.com>
2025-02-26 00:25:07 +0700

    Delete gs-securing-web-extend/initial directory

--- gs-securing-web-extend/initial/src/main/java/com/example/securingweb/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.securingweb.repository;
-
-import com.example.securingweb.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- gs-securing-web-extend/initial/src/main/java/com/example/securingweb/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.securingweb.repository;
-
-import com.example.securingweb.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
