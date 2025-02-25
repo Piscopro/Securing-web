8ffdb81df05ff9d72e9332f9088c2e601dc13307
Piscopro <80594224+Piscopro@users.noreply.github.com>
2025-02-26 00:23:52 +0700

    Add files via upload

---
+++ initial/src/main/java/com/example/securingweb/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.securingweb.repository;
+
+import com.example.securingweb.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
